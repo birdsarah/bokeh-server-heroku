400c2529e122ea837e74ddfbed8cfea3c4a620a9
Sarah Bird <sarah@bonvaya.com>
2015-02-22 22:05:35 -0800

    Remove extra line

--- app.py
+++ app.py
@@ -5,4 +5,3 @@ from bokeh.server.configure import make_tornado_app, register_blueprint
 configure_flask(config_file='config.py')
 register_blueprint()
 tornado_app = make_tornado_app(flask_app=app)
-
