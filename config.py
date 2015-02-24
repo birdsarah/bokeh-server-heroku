aaa4cc513003d06f7fb0479e271585f64a344d85
Sarah Bird <sarah@bonvaya.com>
2015-02-24 01:45:06 -0800

    Add blueprint for sliders example

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os, urlparse
 redis_url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL', '127.0.0.1:6397'))
 
-pub_zmqaddr ="ipc:///tmp/0"
-sub_zmqaddr ="ipc:///tmp/1"
+pub_zmqaddr = "ipc:///tmp/0"
+sub_zmqaddr = "ipc:///tmp/1"
 run_forwarder = True
 model_backend = {'type' : 'redis',
                  'redis_host': redis_url.hostname,
@@ -11,3 +11,6 @@ model_backend = {'type' : 'redis',
                  'start-redis' : False}
 secret_key = os.environ.get('BOKEH_SECRET_KEY', 'another secret key')
 multi_user = False
+scripts = [
+    'blueprints/sliders_app_hbox.py',
+]
