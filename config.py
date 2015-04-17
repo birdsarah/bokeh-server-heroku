6e3c2bf09fe824dfa74f958999986a9bed896265
Sarah Bird <sarah@bonvaya.com>
2015-04-17 01:26:52 -0400

    Update requirements and config

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ secret_key = os.environ.get('BOKEH_SECRET_KEY', 'another secret key')
 multi_user = False
 scripts = [
 #    'blueprints/sliders_app_hbox.py',
-#    'blueprints/washmap_app.py',
+    'blueprints/washmap_app.py',
 ]
