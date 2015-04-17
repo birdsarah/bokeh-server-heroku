1e635876cdc2ae217a85f91991565f8ef9c7ea51
Sarah Bird <sarah@bonvaya.com>
2015-04-16 20:02:11 -0400

    Remove multiuserblaze

--- config.py
+++ config.py
@@ -4,11 +4,12 @@ redis_url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL', '127.0.0.1:6397')
 pub_zmqaddr = "ipc:///tmp/0"
 sub_zmqaddr = "ipc:///tmp/1"
 run_forwarder = True
-model_backend = {'type' : 'redis',
-                 'redis_host': redis_url.hostname,
-                 'redis_port' : redis_url.port,
-                 'redis_password' : redis_url.password,
-                 'start-redis' : False}
+#model_backend = {'type' : 'redis',
+#                 'redis_host': redis_url.hostname,
+#                 'redis_port' : redis_url.port,
+#                 'redis_password' : redis_url.password,
+#                 'start-redis' : False}
+model_backend = {'type': 'shelve'}
 secret_key = os.environ.get('BOKEH_SECRET_KEY', 'another secret key')
 multi_user = False
 scripts = [
